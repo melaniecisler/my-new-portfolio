c23e445af4ff71b464eac24a311fc9d29394edff
melaniecisler <65982774+melaniecisler@users.noreply.github.com>
2020-07-27 16:04:07 -0500

    activity-7 solved

---
+++ Week4/activity-7/unsolved/script.js
@@ -0,0 +1,22 @@
+
+//h1 element
+ var h1Tag = document.createElement("h1");
+h1Tag.textContent = "This is an h1 tag";
+document.body.appendChild(h1Tag);
+ 
+//h2 element
+var h2Tag = document.createElement("h2");
+h2Tag.textContent = "This is an h2 tag";
+document.body.appendChild(h2Tag);
+
+//var img = document.createElement("img");
+//img.setAttribute("src","https://www.google.com/search?q=water+image&sxsrf=ALeKk007q1fMNv2gcRBIm1A1V_uBcKSgJw:1595295820091&tbm=isch&source=iu&ictx=1&fir=lfpXoQOhuzJ7yM%252CTE4Qh6gXsVlEhM%252C_&vet=1&usg=AI4_-kQSBcEehPZtKRj9orYHjIfbpeAOoA&sa=X&ved=2ahUKEwik4auFnN3qAhVPHM0KHdBiAlMQ9QEwAHoECAcQMA#imgrc=lfpXoQOhuzJ7yM" );
+
+var p = document.createElement("p");
+p.textContent = "water";
+document.body.appendChild(p);
+
+var li1 = document.createElement('li');
+li1.textContent = 'steak';
+document.body.appendChild(li1);
+
